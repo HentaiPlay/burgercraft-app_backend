b5fb0b8f93622220663c8fe47ec4bbb0b9842875
HentaiPlay <david.tonoyan16@mail.ru>
2023-08-27 20:15:03 +0300

    Добавлен TypeOrm, добавлен модудь с пользователями и сопутствующий ресурс

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [
+        User
+      ],
+      synchronize: true,
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
