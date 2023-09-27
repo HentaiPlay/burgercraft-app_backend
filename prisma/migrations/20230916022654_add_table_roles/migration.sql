66c76553bed9621b2efcb923a67ed70acc0870c7
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-27 23:46:25 +0300

    Добавлены типы ролей

--- prisma/migrations/20230916022654_add_table_roles/migration.sql
+++ prisma/migrations/20230916022654_add_table_roles/migration.sql
@@ -1,8 +1,14 @@
+-- CreateEnum
+CREATE TYPE "RoleType" AS ENUM ('admin', 'crafter');
+
 -- CreateTable
 CREATE TABLE "roles" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
+    "name" "RoleType" NOT NULL,
     "access_list" JSONB NOT NULL,
 
     CONSTRAINT "roles_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "roles_name_key" ON "roles"("name");
