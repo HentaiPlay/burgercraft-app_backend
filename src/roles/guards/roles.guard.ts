f6797c4b3a3f6c446656624e6bc97041af4d100c
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-27 11:58:27 +0300

    Добавлен гвард для ролевого доступа

---
+++ src/roles/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from "@nestjs/common";
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    try {
+      const roles = this.reflector.getAllAndOverride<string[]>('roles', [context.getHandler(), context.getClass()]);
+      if (!roles) {
+        return false;
+      }
+      const request = context.switchToHttp().getRequest();
+      const userRoles = request.headers?.role?.split(',');
+      return this.validateRoles(roles, userRoles);
+    } catch (e) {
+      console.log(e)
+      throw new HttpException('Нет доступа для данной роли', HttpStatus.FORBIDDEN)
+    }
+  }
+
+  validateRoles(roles: string[], userRoles: string[]) {
+    return roles.some(role => userRoles.includes(role));
+  }
+}
