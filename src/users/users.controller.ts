8a36eaaca2b80a4d39d9a32a7f255ff1d602dc20
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-28 03:58:42 +0300

    Добавлен роут для обновления пользовательского аватара, добавлена валидация на загрузку изображений

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,12 +2,23 @@ import {
   Controller,
   Post,
   Body,
+  Put,
   Patch,
   Param,
   Delete,
   HttpCode,
   UseGuards,
   ParseIntPipe,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
+  Req,
+  HttpException,
+  HttpStatus,
+  BadRequestException,
+  // BadRequestException
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,6 +27,11 @@ import { JWTAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { Roles } from 'src/utilities/decorators/roles';
 import { RolesGuard } from 'src/roles/guards/roles.guard';
 import { Role } from 'src/utilities/types';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { AvatarFileInterceptorOptions } from 'src/pipes/validation.files';
+import { UnsupportedMediaTypeException } from '@nestjs/common';
 
 @Controller('users')
 @UseGuards(JWTAuthGuard)
@@ -34,6 +50,20 @@ export class UsersController {
     @Body() updateUserDto: UpdateUserDto,
   ) {
     return this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Put('avatar/:id')
+  @HttpCode(201)
+  @UseInterceptors(FileInterceptor('file', AvatarFileInterceptorOptions))
+  uploadAvatar(
+    @Req() req: any,
+    @Param('id', ParseIntPipe) id: number,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if (!file || req.fileValidationError) {
+      throw new BadRequestException(req.fileValidationError);
+    }
+    return this.usersService.uploadAvatar(id, file);
   }
 
   @Roles(Role.ADMIN)
