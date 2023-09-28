307bfbeb1c7fcd572836eb03698c3929923ca2fe
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-28 04:06:35 +0300

    Изменен путь до дефолтного аватара в схеме и миграции

--- prisma/migrations/20230916023156_add_table_users/migration.sql
+++ prisma/migrations/20230916023156_add_table_users/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "avatar_path" TEXT NOT NULL DEFAULT 'files/avatar/default.png',
+    "avatar_path" TEXT NOT NULL DEFAULT 'files/avatars/default.png',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
