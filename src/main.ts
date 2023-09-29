66696fd29cfba7b4306863f430a789c8e1d26047
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-29 22:12:05 +0300

    Добавлен ендпоинт для статики медиа

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const PORT = process.env.APP_API_PORT || 3000;
@@ -8,6 +10,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.use('/api/images', express.static(join(__dirname, '../files/images/static')));
 
   await app.listen(PORT);
 }
