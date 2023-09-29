f25b47005e5c4e2e63140e918fefaef2266a2cca
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-29 07:13:03 +0300

    Добавлены данные для сидов продуктов

---
+++ prisma/seeds/models/products/drinks.ts
@@ -0,0 +1,29 @@
+const type = 'drink'
+const photosPath = 'files/products/drinks'
+
+export const drinks = [
+  {
+    name: 'Coca Cola',
+    type: type,
+    photoPath: `${photosPath}/coca_cola.jpg`,
+    price: 35
+  },
+  {
+    name: 'Fanta',
+    type: type,
+    photoPath: `${photosPath}/fanta.jpg`,
+    price: 35
+  },
+  {
+    name: 'Sprite',
+    type: type,
+    photoPath: `${photosPath}/sprite.jpg`,
+    price: 35
+  },
+  {
+    name: 'Мохито',
+    type: type,
+    photoPath: `${photosPath}/mojito.jpg`,
+    price: 45
+  }
+]
---
+++ prisma/seeds/models/products/snacks.ts
@@ -0,0 +1,17 @@
+const type = 'snack'
+const photosPath = 'files/products/snacks'
+
+export const snacks = [
+  {
+    name: 'Картошка фри',
+    type: type,
+    photoPath: `${photosPath}/country_style_potatoes.jpg`,
+    price: 50
+  },
+  {
+    name: 'Картошка по деревенски',
+    type: type,
+    photoPath: `${photosPath}/french_fries.jpg`,
+    price: 55
+  }
+]
