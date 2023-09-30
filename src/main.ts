da4add40f15d2bdc3560e63e15abd6ba3f2dfe89
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-30 22:16:33 +0300

    Замена ValidationPipe на родной из пакета, старая валидация удалена

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './pipes/validation.pipe';
+import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { join } from 'path';
 
