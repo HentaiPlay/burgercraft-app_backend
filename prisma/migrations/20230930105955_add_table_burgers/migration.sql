cbbf9074a5c5e75712af8c709ff3b7c292f0fe1a
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-30 14:17:45 +0300

    Добавлена мигарция на создание таблицы burgers

---
+++ prisma/migrations/20230930105955_add_table_burgers/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "burgers" (
+    "id" SERIAL NOT NULL,
+    "ingredients" JSONB NOT NULL,
+    "price" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "burgers_pkey" PRIMARY KEY ("id")
+);
