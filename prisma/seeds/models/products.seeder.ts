bf6a57bd2cb0a6bc8629e50a81006db643a2055e
HentaiPlay <david.tonoyan16@mail.ru>
2023-09-30 21:52:09 +0300

    Добавлена генерация slug-а для products

--- prisma/seeds/models/products.seeder.ts
+++ prisma/seeds/models/products.seeder.ts
@@ -3,6 +3,7 @@ import { burgerIngredients } from "./products/burger-ingredients";
 import { drinks } from "./products/drinks";
 import { sauces } from "./products/sauces";
 import { snacks } from "./products/snacks";
+import { generateSlug } from "../../../src/utilities/helpers/slug-generator";
 
 class ProductsSeeder extends Seeder {
   
@@ -10,9 +11,10 @@ class ProductsSeeder extends Seeder {
     await this.prisma.product.deleteMany();
 
     const products = [].concat(burgerIngredients, snacks, sauces, drinks)
-    products.forEach(async item => {
+    products.forEach(async product => {
+      product.slug = generateSlug(product.name)
       await this.prisma.product.create({
-        data: item
+        data: product
       })
     })
 
