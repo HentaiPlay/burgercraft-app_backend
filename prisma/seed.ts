4fd85303c6c6645c34d9cb8c2d629f5710917a0d
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-01 00:11:53 +0300

    Добавлено сидирование products

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,10 +4,9 @@ import { SeederFactory } from './seeds/seeder.factory';
 const prisma = new PrismaClient();
 
 async function main() {
-
   const seeder = new SeederFactory(prisma)
   await seeder.run('roles')
-
+  await seeder.run('products')
 }
 
 main()
