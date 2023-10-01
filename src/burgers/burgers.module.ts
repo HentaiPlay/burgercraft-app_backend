b144b19f266d175a44b1d756bef8c926b8b4ae6c
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-01 04:46:40 +0300

    Импортирован prisma модуль

--- src/burgers/burgers.module.ts
+++ src/burgers/burgers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BurgersService } from './burgers.service';
 import { BurgersController } from './burgers.controller';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BurgersController],
-  providers: [BurgersService]
+  providers: [BurgersService],
 })
 export class BurgersModule {}
