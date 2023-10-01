4ce35cbb60614d1eaba2b6b50f132e66bea76e19
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-02 02:24:04 +0300

    Обновлен путь импорта типов

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
-import { ProductType } from 'src/utilities/types';
+import { ProductType } from './types/products.types';
 
 @Injectable()
 export class ProductsService {
