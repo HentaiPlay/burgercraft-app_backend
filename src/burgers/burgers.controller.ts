6d83019a70f76386dde69adf1bf20be709ca7960
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-01 05:08:58 +0300

    Добавлены роуты для создания и удаления burgers

--- src/burgers/burgers.controller.ts
+++ src/burgers/burgers.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { BurgersService } from './burgers.service';
+import { CreateBurgerDto } from './dto/create-burger.dto';
 
 @Controller('burgers')
 export class BurgersController {
   constructor(private readonly burgersService: BurgersService) {}
+
+  @Post('create')
+  @HttpCode(201)
+  async createBurger(@Body() burgerData: CreateBurgerDto) {
+    return await this.burgersService.createBurger(burgerData);
+  }
+
+  @Delete(':id')
+  async deleteBurger(@Param('id', ParseIntPipe) id: number) {
+    await this.burgersService.deleteBurger(id);
+  }
 }
