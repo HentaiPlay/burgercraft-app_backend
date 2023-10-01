a9e901491d318beb2b87ef67baf383b61c200f43
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-02 01:57:38 +0300

    Обновлен enum типа для ингридиента

--- src/burgers/dto/burger-ingredients.dto.ts
+++ src/burgers/dto/burger-ingredients.dto.ts
@@ -1,18 +1,15 @@
 import { IsString, IsInt, IsNotEmpty, IsEnum } from 'class-validator';
+import { BurgerIngredientType } from 'src/products/types/products.types';
 
-export enum TypeIngredient {
-  burgerIngredient = 'burger_ingredient'
-}
-
-export class BurgerIngredientDto  {
+export class BurgerIngredientDto {
   @IsNotEmpty({ message: 'Обязательное поле'})
   @IsInt({ message: 'Должно быть числом' })
   id: number;
 
   @IsNotEmpty({ message: 'Обязательное поле'})
   @IsString({ message: 'Должно быть строкой' })
-  @IsEnum(TypeIngredient, { message: 'Тип не соответствует ингредиенту' })
-  type: TypeIngredient;
+  @IsEnum(BurgerIngredientType, { message: 'Тип не соответствует ингредиенту' })
+  type: BurgerIngredientType;
 
   @IsNotEmpty({ message: 'Обязательное поле'})
   @IsString({ message: 'Должно быть строкой' })
