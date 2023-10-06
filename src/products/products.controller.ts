d29e5f679478222bacc441aa92e99ece9937491c
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-07 02:50:49 +0300

    Добавлен jwt guard для ProductsController

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JWTAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @ApiTags('ProductController')
+@UseGuards(JWTAuthGuard)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseGuards(JWTAuthGuard)
   @ApiOperation({ summary: 'Получение всех продуктов' })
   @Get()
   getAllProducts() {
