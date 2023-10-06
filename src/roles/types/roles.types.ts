5461d0c9c3ca6cd1eba46289a8ad8ed4b4b12560
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 16:36:13 +0300

    Добавлен interface ролей для AuthData

--- src/roles/types/roles.types.ts
+++ src/roles/types/roles.types.ts
@@ -1,4 +1,12 @@
+import { RoleType } from "@prisma/client"
+
 export enum Role {
   ADMIN = 'admin',
   CRAFTER = 'crafter',
+}
+
+export interface IRoleData {
+  id: number,
+  name: RoleType,
+  accessList: Object
 }
