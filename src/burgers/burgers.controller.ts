e506f6683d35c6a7ca116b3b227fee195558f636
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 19:12:27 +0300

    Добавлен swagger

--- src/burgers/burgers.controller.ts
+++ src/burgers/burgers.controller.ts
@@ -12,27 +12,33 @@ import {
 import { BurgersService } from './burgers.service';
 import { CreateBurgerDto } from './dto/create-burger.dto';
 import { UpdateBurgerDto } from './dto/update-burger.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('BurgerController')
 @Controller('burgers')
 export class BurgersController {
   constructor(private readonly burgersService: BurgersService) {}
 
+  @ApiOperation({ summary: 'Получение бургера по id' })
   @Get(':id')
   async getBurger (@Param('id', ParseIntPipe) id: number) {
     return await this.burgersService.findById(id)
   }
 
+  @ApiOperation({ summary: 'Создание бургера' })
   @Post('create')
   @HttpCode(201)
   async createBurger(@Body() burgerData: CreateBurgerDto) {
     return await this.burgersService.createBurger(burgerData);
   }
 
+  @ApiOperation({ summary: 'Редактирование бургера' })
   @Patch()
   async updateBurger (@Body() burgerData: UpdateBurgerDto) {
     return await this.burgersService.updateBurger(burgerData)
   }
 
+  @ApiOperation({ summary: 'Удаление бургера' })
   @Delete()
   async deleteBurger(@Body() burgerData: UpdateBurgerDto) {
     await this.burgersService.deleteBurger(burgerData.id);
