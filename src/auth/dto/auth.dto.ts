e319ce861f088dd23f36a022158116732a4402b1
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 16:42:51 +0300

    Добавлены ApiProperty для swagger-a

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length, IsNotEmpty } from 'class-validator';
 
 export class AuthDto {
+  @ApiProperty({ example: 'cratfer98', description: 'Никнейм пользователя'})
   @IsNotEmpty({ message: 'Обязательное поле' })
   @IsString({ message: 'Должно быть строкой' })
   @Length(3, 20, { message: 'Не меньше 3 и не больше 20 символов' })
   name: string;
 
+  @ApiProperty({ example: 'test_password_123', description: 'Пароль пользователя'})
   @IsNotEmpty({ message: 'Обязательное поле' })
   @IsString({ message: 'Должно быть строкой' })
   @Length(8, 20, { message: 'Не меньше 8 и не больше 20 символов' })
--- src/auth/dto/token.dto.ts
+++ src/auth/dto/token.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class TokenDto {
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NjU5NjU5OSwiZXhwIjoxNjk2NjAwMTk5fQ.1X4hM8DrwVGpMuiGLM3bXUmhS28GJmX79fka1VPEGJ8',
+    description: 'Рефреш токен для получения новой пары'
+  })
   @IsNotEmpty({ message: 'Обязательное поле' })
   @IsString({ message: 'Должно быть строкой' })
   token: string;
