49639c3684fb45b0fb40c274ce1c8ed4461cf848
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 18:23:20 +0300

    Добавлен id как обязательный параметр

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsInt } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty({ message: 'Обязательное поле' })
+  @IsInt({ message: 'Должно быть числом' })
+  id: number;
+}
