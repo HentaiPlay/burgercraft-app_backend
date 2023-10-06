dddda4ad780080f038d5bceda6b2c918221eba0d
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 18:41:13 +0300

    Добавлен swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('ProductController')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiOperation({ summary: 'Получение всех продуктов' })
   @Get()
   getAllProducts() {
     return this.productsService.getAll();
