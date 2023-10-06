e9b0691e0b3771d8aca0a35d5e4bfc65e4cd33b2
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 18:48:55 +0300

    Добавлен ApiProperty для id

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { IsNotEmpty, IsInt } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({example: 1, description: 'Идентификатор пользователя' })
   @IsNotEmpty({ message: 'Обязательное поле' })
   @IsInt({ message: 'Должно быть числом' })
   id: number;
