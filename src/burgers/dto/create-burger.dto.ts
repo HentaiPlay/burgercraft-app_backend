d2a1b33d64760fb153679568eba3eaa6765ba46a
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 19:11:50 +0300

    Добавлены ApiProperty для swagger-a

--- src/burgers/dto/create-burger.dto.ts
+++ src/burgers/dto/create-burger.dto.ts
@@ -9,8 +9,19 @@ import {
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { BurgerIngredientDto } from './burger-ingredients.dto';
+import { ApiProperty } from '@nestjs/swagger';
+
+const exampleIngredients = [
+  { id: 1, type: "burger_ingredient", slug: "verhnyaya_bulochka" },
+  { id: 2, type: "burger_ingredient", slug: "marinovannye_ogurcy" },
+  { id: 4, type: "burger_ingredient", slug: "bekon" },
+  { id: 3, type: "burger_ingredient", slug: "kurinyy_bifshteks" },
+  { id: 12, type: "burger_ingredient", slug: "kapustnyy_list" },
+  { id: 16, type: "burger_ingredient", slug: "nizhnyaya_bulochka" }
+]
 
 export class CreateBurgerDto {
+  @ApiProperty({ example: exampleIngredients, description: 'Список ингредиентов' })
   @IsNotEmpty({ message: 'Обязательное поле' })
   @IsArray({ message: 'Должно быть массивом' })
   @ArrayMinSize(6, { message: 'Должно быть как минимум 6 ингредиентов' })
--- src/burgers/dto/update-burger.dto.ts
+++ src/burgers/dto/update-burger.dto.ts
@@ -1,8 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsInt } from 'class-validator';
 import { CreateBurgerDto } from './create-burger.dto';
 
 export class UpdateBurgerDto extends PartialType(CreateBurgerDto) {
+  @ApiProperty({ example: 1, description: 'Идентификатор бургера' })
   @IsNotEmpty({ message: 'Обязательное поле' })
   @IsInt({ message: 'Должно быть числом' })
   id: number;
