31fad27a3285df7a17b5244fcba19cf6c89d356d
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-07 02:31:22 +0300

    Добавлен тип payload-а для токена

--- src/auth/types/auth.types.ts
+++ src/auth/types/auth.types.ts
@@ -1,4 +1,11 @@
+import { RoleType } from "@prisma/client"
 import { IUserData } from "src/users/types/users.types"
+
+export type TokenPayload = {
+  userId: number,
+  name: string
+  role: RoleType,
+}
 
 export interface ITokens {
   accessToken: string,
