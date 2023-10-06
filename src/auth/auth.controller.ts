ae2445f7105e926f9a6a83af2953eb9ea1a3480b
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 16:59:06 +0300

    Добавлен swagger, добавлена типизация овтетов

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,33 +4,42 @@ import {
   Body,
   HttpCode,
   UseGuards,
-  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { TokenDto } from './dto/token.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IAuthData, ITokens } from './types/auth.types';
+import { AuthDto } from './dto/auth.dto';
 
+@ApiTags('AuthController')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Регистрация' })
+  @ApiResponse({ status: 200, type: Promise<IAuthData> })
   @HttpCode(200)
   @Post('register')
-  async register(@Body() userData: CreateUserDto) {
+  async register(@Body() userData: CreateUserDto): Promise<IAuthData> {
     return this.authService.registration(userData);
   }
 
+  @ApiOperation({ summary: 'Авторизация' })
+  @ApiResponse({ status: 200, type: Promise<IAuthData> })
   @UseGuards(LocalAuthGuard)
   @HttpCode(200)
   @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req);
+  async login(@Body() authData: AuthDto): Promise<IAuthData> {
+    return this.authService.login(authData);
   }
 
+  @ApiOperation({ summary: 'Обновление токена' })
+  @ApiResponse({ status: 200, type: Promise<ITokens> })
   @HttpCode(200)
   @Post('refresh')
-  async getNewToken(@Body() refreshToken: TokenDto) {
+  async getNewToken(@Body() refreshToken: TokenDto): Promise<ITokens> {
     return this.authService.getNewToken(refreshToken);
   }
 }
