9215388b74bab000ae76084c7ff7b2ad16e64e8b
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-06 16:42:15 +0300

    Добавлены ApiProperty для swagger-a

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsInt, IsOptional, IsDate } from 'class-validator';
 import { AuthDto } from 'src/auth/dto/auth.dto';
 
 export class CreateUserDto extends AuthDto {
+  @ApiProperty({ example: 'default.png', description: 'При создании пользователя нельзя выбрать аватар, поэтому устновится дефолтный'})
   @IsOptional()
   @IsString({ message: 'Должно быть строкой' })
   avatar: string;
 
+  @ApiProperty({ example: 1, description: 'Id роли пользователя'})
   @IsOptional()
   @IsInt({ message: 'Должно быть числом' })
   roleId: number;
