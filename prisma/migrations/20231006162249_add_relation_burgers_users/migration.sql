6e074e7062b15182edb5a07216dca53d9ae97d83
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-07 03:53:09 +0300

    Добавлена миграция на создание реляции таблиц burgers и users

---
+++ prisma/migrations/20231006162249_add_relation_burgers_users/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `crafter_id` to the `burgers` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "burgers" ADD COLUMN     "crafter_id" INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "burgers" ADD CONSTRAINT "burgers_crafter_id_fkey" FOREIGN KEY ("crafter_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
