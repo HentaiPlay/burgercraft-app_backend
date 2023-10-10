d12f1eec3ad08fab8d57027007dbecca25e71cd9
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-10 07:13:45 +0300

    Добавлен dto для редактирования продуктов

---
+++ src/products/dto/update-product.dto.ts
@@ -0,0 +1,11 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsInt } from 'class-validator';
+import { CreateProductDto } from './create-product.dto';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiProperty({example: 1, description: 'Идентификатор пользователя' })
+  @IsNotEmpty({ message: 'Обязательное поле' })
+  @IsInt({ message: 'Должно быть числом' })
+  id: number;
+}
