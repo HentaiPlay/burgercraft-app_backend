3145a95532a856fa6449724f75252db517681ba8
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-10 07:12:46 +0300

    Добавлен dto для загрузки изображений продуктов

---
+++ src/products/dto/upload-photo.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEnum, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+import { ProductType, ProductTypes } from '../types/products.types';
+
+export class UploadPhotoDto {
+  @ApiProperty({ example: 'Coca Cola', description: 'Название продукта'})
+  @IsNotEmpty({ message: 'Обязательное поле' })
+  @IsString({ message: 'Должно быть строкой' })
+  name: string
+
+  @ApiProperty({ example: 'drink', description: 'Параметр для типа продукта'})
+  @IsNotEmpty({ message: 'Обязательное поле' })
+  @IsString({ message: 'Должно быть строкой' })
+  @IsEnum(ProductTypes, { message: 'Тип продукта не существует' })
+  type: ProductType;
+
+  @ApiProperty({ example: 'coca_cola.jpg', description: 'Название сохраненного файла'})
+  @IsNotEmpty({ message: 'Обязательное поле' })
+  @IsString({ message: 'Должно быть строкой' })
+  fileName: string;
+}
