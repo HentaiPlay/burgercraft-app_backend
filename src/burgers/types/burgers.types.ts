83f86de75ce83d5a6b291c9e505e4940db99a1f8
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-10 10:57:21 +0300

    Добавлено свойство isSaled

--- src/burgers/types/burgers.types.ts
+++ src/burgers/types/burgers.types.ts
@@ -2,5 +2,6 @@ import { BurgerIngredientOptions } from "src/products/types/products.types"
 
 export type Burger = {
   price: number,
+  isSaled: boolean,
   ingredients: Array<BurgerIngredientOptions>
 }
