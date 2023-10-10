54444806e444f7ae484e1f8df64709b8db3f90b5
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-10 10:54:18 +0300

    Добавлено поле is_saled для таблицы burgers

--- prisma/migrations/20230930105955_add_table_burgers/migration.sql
+++ prisma/migrations/20230930105955_add_table_burgers/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "burgers" (
     "id" SERIAL NOT NULL,
     "price" INTEGER NOT NULL,
+    "is_saled" BOOLEAN NOT NULL DEFAULT false,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "burgers_pkey" PRIMARY KEY ("id")
