22f3ba4d06702f111acc8aec093d62c1866d3407
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-15 01:17:34 +0300

    Изменен тип Burger, добавлен типа BurgerIngredient (тип возвращаемый из промежуточной таблицы)

--- src/burgers/types/burgers.types.ts
+++ src/burgers/types/burgers.types.ts
@@ -2,6 +2,12 @@ import { BurgerIngredientOptions } from "src/products/types/products.types"
 
 export type Burger = {
   price: number,
-  isSaled: boolean,
   ingredients: Array<BurgerIngredientOptions>
+  orderId: number
+}
+
+export type BurgerIngredient = {
+  id: number,
+  burgerId: number,
+  ingredientId: number
 }
