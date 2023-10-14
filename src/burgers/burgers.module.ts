c4b2aade7897bfef6e58def955a0f9b6c78ee436
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-15 01:34:19 +0300

    Импортирован ordersModule как циклическая зависимость

--- src/burgers/burgers.module.ts
+++ src/burgers/burgers.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { BurgersService } from './burgers.service';
 import { BurgersController } from './burgers.controller';
 import { PrismaModule } from 'prisma/prisma.module';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    forwardRef(() => OrdersModule)
+  ],
   controllers: [BurgersController],
   providers: [BurgersService],
+  exports: [BurgersService]
 })
 export class BurgersModule {}
