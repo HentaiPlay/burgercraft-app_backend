51faaf37485c29576e67c275ba63493f5fd68015
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-15 23:24:56 +0300

    Импортирован StatsModule

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,12 +4,14 @@ import { OrdersController } from './orders.controller';
 import { PrismaModule } from 'prisma/prisma.module';
 import { BurgersModule } from 'src/burgers/burgers.module';
 import { ProductsModule } from 'src/products/products.module';
+import { StatsModule } from 'src/stats/stats.module';
 
 @Module({
   imports: [
     PrismaModule,
     forwardRef(() => BurgersModule),
     ProductsModule,
+    StatsModule
   ],
   controllers: [OrdersController],
   providers: [OrdersService],
