9483b636934d4bde179a289b9dc09a2782693676
HentaiPlay <david.tonoyan16@mail.ru>
2023-10-16 16:06:26 +0300

    Добавлены data-наполнители сидов статистики

---
+++ prisma/seeds/stats/data/stats.ts
@@ -0,0 +1,18 @@
+interface IStats {
+  summ: number
+  amountOrders: number
+  crafterId: number
+}
+
+export const stats: IStats[] = [
+  {
+    summ: 367,
+    amountOrders: 1,
+    crafterId: 2
+  },
+  {
+    summ: 564,
+    amountOrders: 2,
+    crafterId: 3
+  }
+]
