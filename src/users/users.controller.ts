89ebec7dcccf1a87e88a21125b2938d0c6fb5dfe
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-05 21:31:31 +0300

    Удален ролевой доступ к функции удалении профиля, изменен формат dto для сервиса

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Get,
-  Body,
   Put,
   Patch,
   Delete,
@@ -16,9 +15,6 @@ import {
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JWTAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { Roles } from 'src/utilities/decorators/roles';
-import { RolesGuard } from 'src/roles/guards/roles.guard';
-import { Role } from 'src/roles/types/roles.types'; 
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AvatarFileInterceptorOptions } from 'src/utilities/interceptors/images.interceptor';
 import { createReadStream } from 'fs';
@@ -73,10 +69,8 @@ export class UsersController {
   }
 
   @ApiOperation({ summary: 'Удаление пользователя' })
-  @Roles(Role.ADMIN)
-  @UseGuards(RolesGuard)
   @Delete()
-  remove(@Body() updateUserDto: UpdateUserDto): Promise<void> {
-    return this.usersService.deleteUser(updateUserDto);
+  remove(@Request() req): Promise<void> {
+    return this.usersService.deleteUser({ id: Number(req.user.id) });
   }
 }
