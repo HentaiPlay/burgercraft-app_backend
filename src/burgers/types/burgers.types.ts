8fcaa34f87eb94da797e7c621bf27b56382bde54
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-19 04:51:19 +0300

    Добавлено свойство id в тип Burger

--- src/burgers/types/burgers.types.ts
+++ src/burgers/types/burgers.types.ts
@@ -1,7 +1,8 @@
 import { BurgerIngredientOptions } from "src/products/types/products.types"
 
 export type Burger = {
-  price: number,
+  id: number
+  price: number
   ingredients: Array<BurgerIngredientOptions>
   orderId: number
 }
